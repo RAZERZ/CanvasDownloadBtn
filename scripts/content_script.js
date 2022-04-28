d598b350ff816266e2ffd0baf396f2e15c093554
Rami Abou Zahra <ramiabouzahra@gmail.com>
2022-04-28 18:17:35 +0200

    Added half-assed get all btn

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,5 +1,56 @@
 //Define general functions
 
+contextModuleDownloader = listElements => {
+
+  for(j=0; j< listElements.length; j++) {
+    
+    if(listElements[j].getAttribute("class") !== null) {
+
+      if(listElements[j].querySelectorAll('[title="Attachment"]').length) {attachmentCounter++;}
+
+      //In modules, the file name is stored as a class name in the list tag, just adding "download" to the "a" tag will download HTML
+
+      downloadA = listElements[j].getElementsByTagName("a");
+      listClasses = listElements[j].getAttribute("class").match(/\S+/g)||[]
+
+      if((listElements[j].getAttribute("class").match(/\S+/g)||[]).findIndex(element => element.includes("Attachment")) > 0) {
+        attachmentIndex = (listClasses).findIndex(element => element.includes("Attachment"));
+        fileIndexName = listClasses[attachmentIndex].replace("Attachment_", "");
+        
+        if(downloadA.length > 1) {
+
+          fileLocationBuilder = downloadA[1].getAttribute("href").split("/").slice(0,3);
+          fileLocationBuilder.push("files");
+          fileLocationBuilder.push(fileIndexName); 
+          fileLocationBuilder.push("download?download_frd=1");
+
+          downloadA[1].setAttribute("href", fileLocationBuilder.join("/"))
+
+          addDownloadOnClick(downloadA[1]);
+        }
+
+      }
+
+    }
+  }
+
+}
+
+getAll = element => {
+
+  contextModuleParent = element.path[2];
+  console.log()
+  attachments = contextModuleParent.getElementsByClassName("content")[0].querySelectorAll("ul.context_module_items")[0].querySelectorAll("li.attachment");
+
+  for(i=0; i< attachments.length; i++) {
+
+    attachments[i].getElementsByTagName("a")[1].click();
+
+  }
+  
+
+}
+
 addDownloadOnClick = element => {
 
   initHTML = element.parentElement.innerHTML;
@@ -68,38 +119,24 @@ window.onload = () => {
       //We want to iterate over the list to get every list object
       contextModuleList = contextModulesContent.getElementsByTagName("ul")[0].getElementsByTagName("li");
 
-      for(j=0; j< contextModuleList.length; j++) {
-        
-        if(contextModuleList[j].getAttribute("class") !== null) {
+      attachmentCounter = 0;
 
-          //In modules, the file name is stored as a class name in the list tag, just adding "download" to the "a" tag will download HTML
+      contextModuleDownloader(contextModuleList);
 
-          downloadA = contextModuleList[j].getElementsByTagName("a");
-          listClasses = contextModuleList[j].getAttribute("class").match(/\S+/g)||[]
+      if(attachmentCounter > 0) {
 
-          if((contextModuleList[j].getAttribute("class").match(/\S+/g)||[]).findIndex(element => element.includes("Attachment")) > 0) {
-            attachmentIndex = (listClasses).findIndex(element => element.includes("Attachment"));
-            fileIndexName = listClasses[attachmentIndex].replace("Attachment_", "");
-            
-            if(downloadA.length > 1) {
+        contextModuleHeader = contextModules[i].getElementsByClassName("ig-header header")[0].getElementsByClassName("prerequisites")[0];
 
-              fileLocationBuilder = downloadA[1].getAttribute("href").split("/").slice(0,3);
-              fileLocationBuilder.push("files");
-              fileLocationBuilder.push(fileIndexName); 
-              fileLocationBuilder.push("download?download_frd=1");
+        getAllBtn = document.createElement("span");
+        getAllBtn.innerText = "Get All";
 
-              downloadA[1].setAttribute("href", fileLocationBuilder.join("/"))
+        contextModuleHeader.parentElement.insertBefore(getAllBtn, contextModuleHeader);
 
-              addDownloadOnClick(downloadA[1]);
-            }
+        getAllBtn.addEventListener("click", item => {getAll(item)})
 
-          }
-
-        }
       }
 
     }
-
   }
   else {
     console.log("ligma")
